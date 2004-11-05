906612cbef80cf5ffa8108dc87d7603a2cd0caa4
philippe <devnull@localhost>
2004-11-05 18:00:52 +0100

    [svn] Adaptation script python au rebond

--- traite_arbre.py
+++ traite_arbre.py
@@ -2,14 +2,14 @@
 import re
 import sys
 
-def sortie(t,n,l,num) :
-    print n, num, l 
+def sortie(t,n,l,r,num) :
+    print n, num, l,r 
     if t.has_key(n) :
-        filsg,filsd,l,num = t[n] ;
-        sortie(t,filsg,l,num)
-        sortie(t,filsd,l,num)
+        filsg,filsd,l,r,num = t[n] ;
+        sortie(t,filsg,l,r,num)
+        sortie(t,filsd,l,r,num)
 
-p=re.compile(r'^(\S+_\S+)\s+(\S+_\S+)\s+(\S+_\S+)\s+(\S+)$') 
+p=re.compile(r'^(\S+_\S+)\s+(\S+_\S+)\s+(\S+_\S+)\s+(\S+)\s+(\S+)$') 
 
 sys.setrecursionlimit(40000) 
 t={}
@@ -17,6 +17,6 @@ num=1
 for l in sys.stdin.readlines():
     m = p.match(l)
     if m:
-        t[m.group(1)]=(m.group(2),m.group(3),m.group(4),num)
+        t[m.group(1)]=(m.group(2),m.group(3),m.group(4),m.group(5),num)
         num += 1
-sortie(t,m.group(1),0,0)
+sortie(t,m.group(1),0,0,0)
